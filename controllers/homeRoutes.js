28d2ed9ae6608f0a96a7207dc74f8fee06c30780
Guillermo Garcia <ggq0107@hotmail.com>
2021-04-14 19:21:06 -0400

    added get route for one blog per id, not working properly yet

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -33,6 +33,28 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
+router.get('/blog/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const blog = blogData.get({ plain: true });
+
+    res.render('blog', {
+      ...blog,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
